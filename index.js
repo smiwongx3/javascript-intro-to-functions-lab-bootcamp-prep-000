201445133c9f6c07e99695ab050980c368a301b0
Samantha Wong <smiwong920@gmail.com>
2019-01-25 01:12:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ function sayHiToGrandma(string)  {
   {if (string.toLowerCase() === string) 
   {return ("I can't hear you!")}}
 }
+function sayHiToGrandma(string)  {
+  {if (string.toUpperCase() === string) 
+  {return ("YES INDEED!")}}
+}
