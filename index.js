584f90feac7f1ca47333c0d2a7b8be3aaf3171ab
Samantha Wong <smiwong920@gmail.com>
2019-01-25 00:29:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function whisper(string)  {
   return string.toLowerCase()
 }
 function logShout(string)  {
-  return (console.log()).toUpperCase()
+  return console.log(string.toUpperCase()).toUpperCase()
 }
 function logWhisper(string)  {
   return (console.log()).toLowerCase()
