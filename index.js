2490bec69de0f3d3063fd45dd6c824ff7ff0d3f3
Samantha Wong <smiwong920@gmail.com>
2019-01-25 01:07:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function logWhisper(string)  {
 }
 function sayHiToGrandma(string)  {
   if (string.toLowerCase() === string) 
-  console.log("I can't hear you!")
+  return console.log("I can't hear you!")
 }
